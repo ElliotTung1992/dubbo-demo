807b88df53c2108336c813cac025e9f2e9f182cd
dge1992 <dge_1992@163.com>
2018-06-28 22:05:27 +0800

    置空

--- README.md
+++
@@ -1,9 +0,0 @@
-## java8新特性
-
-* Lambda 表达式
-* 方法引用 
-* Stream API
-* Date Time API 
-* 更好的类型推断
-
-主要涉及上面以及关于编程规范的学习
--- lambda/src/main/java/com/github/dge1992/lambda/java8/FunctionalInterface/Transformer.java
+++
@@ -1,8 +0,0 @@
-package com.github.dge1992.lambda.java8.FunctionalInterface;
-
-@FunctionalInterface
-public interface Transformer<T> {
-
-    T transform(T input);
-
-}
